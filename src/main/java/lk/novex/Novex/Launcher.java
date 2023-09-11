f9f74cf271b65a9476b5f5c76aa13811b625e61a
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2023-09-11 09:52:19 +0530

    Commit

--- src/main/java/lk/novex/Novex/Launcher.java
+++ src/main/java/lk/novex/Novex/Launcher.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Launcher extends Application {
@@ -15,8 +16,8 @@ public class Launcher extends Application {
         Parent rootNode = FXMLLoader.load(this.getClass().getResource("/view/managesupplier_form.fxml"));
         Scene scene = new Scene(rootNode);
 
-      /*  Image image = new Image("images/WhatsApp Image 2023-08-07 at 21.08.13-modified.png");
-        stage.getIcons().add(image);*/
+        Image image = new Image("images/WhatsApp Image 2023-08-07 at 21.08.13-modified.png");
+        stage.getIcons().add(image);
 
         stage.setTitle("Login Page");
         stage.centerOnScreen();
