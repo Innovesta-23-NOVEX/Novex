22d0b0cc3d427db18bcbc439bb615f58bb2e0ee0
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2023-09-12 20:53:45 +0530

    with many pages

--- src/main/java/lk/novex/Novex/controller/LoginFormController.java
+++ src/main/java/lk/novex/Novex/controller/LoginFormController.java
@@ -5,20 +5,25 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class LoginFormController {
     @FXML
+    private AnchorPane rootNode;
+
+
+    @FXML
     void btnHomeOnAction(ActionEvent event) throws IOException {
         //Navigate to Home Page
         Parent rootNode = FXMLLoader.load(this.getClass().getResource("/view/home_form.fxml"));
+
         Scene scene = new Scene(rootNode);
-        Stage stage = new Stage();
+        Stage stage = (Stage) this.rootNode.getScene().getWindow();
         stage.setTitle("Recolter");
         stage.setScene(scene);
-        stage.show();
     }
 }
 
